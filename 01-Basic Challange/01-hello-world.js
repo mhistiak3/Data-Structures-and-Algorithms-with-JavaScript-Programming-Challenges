07b0f213305d9935de9cdc59474870131486bd3a
Istiak <mhistiak2@gmail.com>
2024-07-14 18:40:47 +0600

    count occurrences challange completed and edit hello world file

--- 01-Basic Challange/01-hello-world.js
+++ 01-Basic Challange/01-hello-world.js
@@ -1,5 +1,5 @@
 // Challange:01 Write a function called helloWorld that returns a string of 'Hello World!'.
-const helloWord = ()=>{
-    return "Hello World!";
-}
-console.log(helloWord());
+const helloWorld = () => {
+  return "Hello World!";
+};
+console.log(helloWorld());
---
+++ 01-Basic Challange/04-count-occurrences.js
@@ -0,0 +1,15 @@
+// Challange:04 Write a function called countOccurrences() that takes in a string and a character and returns the number of occurrences of that character in the string.
+
+// const countOccurrences = (str,char)=>{
+//     let charCount = 0
+
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === char) {
+//         charCount++
+//       }
+//     }
+//     return charCount
+// }
+const countOccurrences = (str,char)=> str.split(char).length - 1
+
+console.log(countOccurrences("hello world","p"));
