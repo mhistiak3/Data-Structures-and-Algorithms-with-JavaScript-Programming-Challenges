e3a64d9ed8f3a561eab4939d0ae6292807597f93
Istiak <mhistiak2@gmail.com>
2024-07-17 01:15:46 +0600

    palindrome challange completed

--- 01-Basic Challange/08-palindrome.js
+++ 01-Basic Challange/08-palindrome.js
@@ -1,8 +1,41 @@
 // Write a function called `isPalindrome` that takes in a string and returns `true` if the string is a palindrome and `false` if it is not.
 
-const isPalindrome = (str)=>{
-    let reversStr = str.split("").reverse()
-    console.log(reversStr);
-    
+// const isPalindrome = (str) =>{
+//     let formatedStr = str.toLowerCase().replace(/[^a-z0-9]/g,"")
+//     return formatedStr === formatedStr.split("").reverse().join("");
+// }
+
+const isPalindrome = (str) => {
+  let formatedStr = removeNonAlphaNumeric(str.toLowerCase());
+  return formatedStr === reverseStr(formatedStr);
+};
+const removeNonAlphaNumeric = (str) => {
+  let formatedStr = "";
+  for (let i = 0; i < str.length; i++) {
+ 
+    if (isAlphaNumeric(str[i])) {
+        
+      formatedStr += str[i];
+    }
+  }
+  
+  return formatedStr;
+};
+const isAlphaNumeric = (str) => {
+  let code = str.charCodeAt(0);
+  return (code >= 48 && code <= 57) || (code >= 97 && code <= 122);
+};
+
+const reverseStr = (str)=>{
+   if (!str) {
+     return "";
+   }
+   let reverseStr = "";
+   for (let i = str.length - 1; i >= 0; i--) {
+     reverseStr += str[i];
+   }
+   return reverseStr.toLowerCase();
 }
-isPalindrome("hello")
+
+console.log(isPalindrome("Madam %&$")); // true
+console.log(isPalindrome("hello")); // false
