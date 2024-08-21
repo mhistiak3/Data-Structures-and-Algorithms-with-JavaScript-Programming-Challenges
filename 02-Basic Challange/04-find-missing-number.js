0058b8a574800d0db49ae8d98762fb9c6ed8db65
Istiak <mhistiak2@gmail.com>
2024-08-21 23:31:57 +0600

    Update Missing number funtion

--- 02-Basic Challange/04-find-missing-number.js
+++ 02-Basic Challange/04-find-missing-number.js
@@ -16,14 +16,28 @@
 //   }
 // };
 
-const findMissingNumber = (numArr = []) => {
-  const numLen = numArr.length;
-  if (numLen === 0) return 1;
-  let n = numLen + 1;
-  const expectedSum = (n * (n + 1)) / 2;
-  const actualSum = numArr.reduce((acc, value) => acc + value, 0);
+// const findMissingNumber = (numArr = []) => {
+//   const numLen = numArr.length;
+//   if (numLen === 0) return 1;
+//   let n = numLen + 1;
+//   const expectedSum = (n * (n + 1)) / 2;
+//   const actualSum = numArr.reduce((acc, value) => acc + value, 0);
 
-  return expectedSum - actualSum
+//   return expectedSum - actualSum
+// };
+
+const findMissingNumber = (numArr = []) => {
+  const n = numArr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = numArr.reduce((acc, num) => acc + num, 0);
+
+  const missingNumber = expectedSum - actualSum;
+
+  // If the missing number is not in the valid range (1 to n), return a message.
+  return missingNumber > 0 && missingNumber < n
+    ? missingNumber
+    : "No number is missing";
 };
 
-console.log(findMissingNumber([1, 2, 3, 4, 6, 5, 8, 9, 10]));
+console.log(findMissingNumber([1, 2, 3, 4, 6, 8, 9, 5]));
+// console.log(findMissingNumber([1, 2, 3, 4, 5]));
