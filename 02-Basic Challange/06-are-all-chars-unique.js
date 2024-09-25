bf505517707c0a37c5851c4cfc96669b9a77a2ff
Istiak <mhistiak2@gmail.com>
2024-09-25 19:06:18 +0600

    06 are all chars unique challange done

---
+++ 02-Basic Challange/06-are-all-chars-unique.js
@@ -0,0 +1,18 @@
+// Write a function called areAllCharactersUnique that takes in a string and returns true or false depending on whether all characters in the string are unique (i.e., no character is repeated).
+
+// const areAllCharactersUnique = (str) =>
+//   [...new Set([...str.split("")])].join("") === str
+
+const areAllCharactersUnique = (str) => {
+  let strArr = str.split("");
+  let newArr = [];
+  for (let i = 0; i < strArr.length; i++) {
+    if (newArr.includes(strArr[i])) {
+      return false;
+    }
+    newArr.push(strArr[i]);
+  }
+  return true;
+};
+
+console.log(areAllCharactersUnique("monir"));
